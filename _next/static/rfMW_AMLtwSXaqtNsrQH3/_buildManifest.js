a0d9b4df5a2193d99c22f7ec1402f54a37c5bb08
JeetChaudhari <JeetChaudhari@users.noreply.github.com>
2021-04-26 08:48:35 +0000

    deploy: 4f2d7010bfec59e6e3915bd7c5ddcd5e0c53675a

--- _next/static/rfMW_AMLtwSXaqtNsrQH3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-f760c14728d6c730fc2c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a95c1df527293683eeef.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
